a0f04ecddd5bd3eb1883fd3d6ce61bdc27b77d55
Nathanael <nathanael@bynder.com>
2021-01-31 14:42:48 +0100

    pytest hydra

--- snowblocks/emacs/emacs/lisp/igloo-python.el
+++ snowblocks/emacs/emacs/lisp/igloo-python.el
@@ -14,10 +14,31 @@
   (setq-default
    python-shell-interpreter "ipython"))
 
+(use-package python-pytest)
+
+(pretty-hydra-define python-hydra-test
+  (:forein-keys warn :color teal :idle 0.5 :quit-key ("q" "<escape>"))
+  ("Hydra python test"
+   (("v" python-pytest-file "python-pytest-file")
+    ("a" python-pytest-dispatch "python-pytest-dispatch")
+    ("f" python-pytest-function "python-pytest-function"))))
+
+(pretty-hydra-define python-hydra-insert
+  (:foreign-keys warn :color teal :idle 0.5 :quit-key ("q" "<escape>"))
+  ("Hydra python insert"
+   (("s" yas-insert-snippet "yas-insert-snippet"))))
+
 (major-mode-hydra-define python-mode
   (:foreign-keys warn :color teal :idle 0.5 :quit-key ("q" "<escape>"))
   ("Eval"
-   (("b" nil "run buffer"))))
+   (("b" nil "run buffer"))
+   "REPL"
+   (("c" nil "open repl"))
+   "Test"
+   (("t" python-hydra-test/body "test-hydra"))
+   "Insert"
+   (("i" python-hydra-insert/body "insert-hydra"))
+   ))
 	    
 
 (provide 'igloo-python)
