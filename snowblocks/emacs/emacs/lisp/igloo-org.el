588f6b4ea618469ea3dd87977c09330b0b0a3527
Nathanaël Leaute <nathanael.leaute@bynder.com>
2021-02-12 10:05:51 +0100

    Org config

---
+++ snowblocks/emacs/emacs/lisp/igloo-org.el
@@ -0,0 +1,9 @@
+;;; igloo-org.el --- Org config -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(add-hook 'org-mode-hook #'auto-fill-mode)
+
+(provide 'igloo-org)
+
+;;; igloo-org.el ends here
