f9f8e77904af0e2b203ccd2bbb5b454d3aabf561
Nathanaël Leaute <nathanael.leaute@bynder.com>
2021-02-19 10:14:05 +0100

    wip

--- snowblocks/emacs/emacs/init.el
+++ snowblocks/emacs/emacs/init.el
@@ -1,5 +1,5 @@
 ;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
-; (setq debug-on-error t)
+(setq debug-on-error t)
 
 ;; Platform --------------------------------------------------------------------
 (defconst IS-MAC     (eq system-type 'darwin))
@@ -106,7 +106,7 @@
  '(custom-safe-themes
    '("99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "77113617a0642d74767295c4408e17da3bfd9aa80aaa2b4eeb34680f6172d71a" "6084dce7da6b7447dcb9f93a981284dc823bab54f801ebf8a8e362a5332d2753" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" "7a994c16aa550678846e82edc8c9d6a7d39cc6564baaaacc305a3fdc0bd8725f" "5036346b7b232c57f76e8fb72a9c0558174f87760113546d3a9838130f1cdb74" "c83c095dd01cde64b631fb0fe5980587deec3834dc55144a6e78ff91ebc80b19" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "2c49d6ac8c0bf19648c9d2eabec9b246d46cb94d83713eaae4f26b49a8183fc4" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" default))
  '(package-selected-packages
-   '(yasnippet-snippets python-pytest exec-path-from-shell pipenv yaml-mode flucui-themes direnv nord-theme python-mode flycheck yasnippet company-box company lsp-python-ms lsp-ui lsp-mode evil-smartparens evil-nerd-commenter csv-mode which-key major-mode-hydra use-package smex persp-mode ivy-rich hydra general counsel)))
+   '(shell-pop nose yasnippet-snippets python-pytest exec-path-from-shell pipenv yaml-mode flucui-themes direnv nord-theme python-mode flycheck yasnippet company-box company lsp-python-ms lsp-ui lsp-mode evil-smartparens evil-nerd-commenter csv-mode which-key major-mode-hydra use-package smex persp-mode ivy-rich hydra general counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- snowblocks/emacs/emacs/lisp/igloo-python.el
+++ snowblocks/emacs/emacs/lisp/igloo-python.el
@@ -2,6 +2,29 @@
 ;;; Commentary:
 ;;; Code:
 
+;; Lib -------------------------------------------------------------------------
+(defun python-pytest--get-buffer ()
+  "Get a create a suitable compilation buffer."
+  (if (eq major-mode 'python-pytest-mode)
+      (current-buffer)  ;; re-use buffer
+    (let ((name python-pytest-buffer-name))
+      (when python-pytest-project-name-in-buffer-name
+        (setq name (format "%s<%s>" name (python-pytest--project-name))))
+      (get-buffer-create name))))
+
+(defun igloo-pytest-run-in-popup ()
+  "Run pytest function cmd in pop window."
+  (interactive)
+  (select-window
+   (display-buffer-in-side-window
+    (save-window-excursion
+      ;; (#'cmd)
+      (python-pytest-file (buffer-file-name))
+      ;; (let ((prefix-arg cmd))
+      ;;   (call-interactively #'cmd))
+      (current-buffer))
+    '((side . bottom)))))
+
 (use-package lsp-python-ms
   :defer 0.3
   :custom
@@ -10,16 +33,19 @@
 (use-package python-mode
   :init
   (setq-default
-   python-shell-interpreter "ipython"))
+   python-shell-interpreter "ipython")
+  (add-hook 'python-mode'hook 'nose-mode))
 
 (use-package python-pytest)
 
 (pretty-hydra-define python-hydra-test
   (:forein-keys warn :color teal :idle 0.5 :quit-key ("q" "<escape>"))
   ("Hydra python test"
-   (("v" python-pytest-file     "python-pytest-file")
-    ("a" python-pytest-dispatch "python-pytest-dispatch")
-    ("f" python-pytest-function "python-pytest-function"))))
+   (("f" python-pytest-file     "python-pytest-file")
+    ("p" python-pytest-dispatch "python-pytest-dispatch")
+    ("t" python-pytest-function "python-pytest-function"))
+   ""
+   (("v" python-pytest-file "python-pytest-file"))))
 
 (pretty-hydra-define python-hydra-insert
   (:foreign-keys warn :color teal :idle 0.5 :quit-key ("q" "<escape>"))
@@ -37,7 +63,8 @@
    "Insert"
    (("i" python-hydra-insert/body "insert-hydra"))
    ))
-	    
+
+
 
 (provide 'igloo-python)
 ;;; igloo-python.el ends here
