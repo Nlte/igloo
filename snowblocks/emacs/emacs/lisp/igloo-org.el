a89759bac70c7c93d9224ff2d7ac94d6894d18e7
Nathanaël Leaute <n.leaute@gmail.com>
2021-03-03 19:58:55 +0100

    autofold org

--- snowblocks/emacs/emacs/lisp/igloo-org.el
+++ snowblocks/emacs/emacs/lisp/igloo-org.el
@@ -2,15 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (pretty-hydra-define org-hydra-date
-;;   (:foreign-keys warn :color teal :idle 0.5 :quit-key ("q" "<escape>"))
-;;   ("Hydra org date"
-;;    (("d" nil "nil"))
-;;    ))
-   ;; (("d" org-deadline "org-deadline"
-   ;;   "s" org-schedule "org-schedule"
-   ;;   "t" org-time-stamp "org-time-stamp"
-   ;;   "T" org-time-stamp-inactive "org-timestamp-inactive"))))
+(setq org-startup-folded t)
 
 (pretty-hydra-define org-hydra-date
   (:foreign-keys warn :color teal :idle 0.5 :quit-key ("q" "<escape>"))
@@ -19,8 +11,6 @@
     ("s" org-schedule "org-schedule")
     ("t" org-time-stamp "org-time-stamp")
     ("T" org-time-stamp-inactive "org-time-stamp-inactive"))))
-
-
 
 (pretty-hydra-define org-hydra-link
   (:foreign-keys warn :color teal :idle 0.5 :quit-key ("q" "<escape>"))
