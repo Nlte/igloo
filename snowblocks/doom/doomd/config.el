ff32222f034f9afbed1e823004eff80c1c9d6abf
Nathanaël Leaute <n.leaute@gmail.com>
2021-03-08 20:53:28 +0100

    clean doomd

--- snowblocks/doom/doomd/config.el
+++ snowblocks/doom/doomd/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Nathanaël Leaute"
+(setq user-full-name "Nlte"
       user-mail-address "n.leaute@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
@@ -22,24 +22,17 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
-(setq doom-font (font-spec :family "Source code pro" :size 12 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Source code pro" :size 12))
-(setq +helm-posframe-text-scale 0)
-
-
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-nord-light)
+(setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-;; Set default directory to $HOME
+;; Change default directory to HOME
 (setq default-directory "~/")
-;;(cd (getenv "HOME")) ;; for windows
-
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -62,119 +55,3 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-
-
-;; Global config
-
-;; Org
-;;
-(setq org-src-tab-acts-natively t)
-(setq org-startup-folded t)
-
-;; Python
-(setq python-shell-interpreter "python")
-
-
-;; Custom modes
-;;
-
-
-;; Package configs
-;;
-;; EIN
-;; (use-package! ein
-;;   :init
-;;   (setq ein:notebook-autosave-frequency 10)
-;;   (setq ein:notebook-create-checkpoint-on-save t))
-;;   :config
-;;   (map! (:when (featurep! :tools ein)
-;;        (:map ein:notebook-mode-map
-;;         :leader
-;;         (:prefix-map ("j" . "jupyter")
-;;         :desc "Execute cell"            "c" #'ein:worksheet-execute-cell-and-goto-next-km
-;;         :desc "Execute all cells"       "R" #'ein:worksheet-execute-all-cells
-;;         :desc "Insert cell below"       "b" #'ein:worksheet-insert-cell-below-km
-;;         :desc "Insert cell above"       "a" #'ein:worksheet-insert-cell-above-km
-;;         :desc "Next cell"               "j" #'ein:worksheet-goto-next-input-km
-;;         :desc "Previous cell"           "k" #'ein:worksheet-goto-prev-input-km
-;;         :desc "Yank cell"               "y" #'ein:worksheet-yank-cell-km
-;;         :desc "Rename notbook"          "w" #'ein:notebook-rename-command-km
-;;         ))))
-;; (add-hook 'kill-emacs-hook 'ein:stop)
-
-;; Neotree
-(setq neo-theme 'ascii)
-
-;; Ivy
-(map! (:leader
-      (:prefix "f"
-       :desc "ivy-project-grep"         "g" #'+ivy:project-search
-       )))
-
-;; mu4e
-(setq +mu4e-mu4e-mail-path "~/.local/share/mail")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
-;; Each path is relative to `+mu4e-mu4e-mail-path', which is ~/.mail by default
-(set-email-account! "n.leaute@gmail.com"
-  '((mu4e-sent-folder       . "/gmail/sent")
-    (mu4e-drafts-folder     . "/gmail/drafts")
-    (mu4e-trash-folder      . "/gmail/trash")
-    (mu4e-refile-folder     . "/gmail/all")
-    (smtpmail-smtp-user     . "n.leaute@gmail.com")
-    (user-mail-address      . "n.leaute@gmail.com")    ;; only needed for mu < 1.4
-    (mu4e-compose-signature . "---\nNathanaël Leaute"))
-  t)
-
-
-;; Magit
-(defun kill-magit-diff-buffer-in-current-repo (&rest _)
-  "Delete the magit-diff buffer related to the current repo"
-  (let ((magit-diff-buffer-in-current-repo
-         (magit-mode-get-buffer 'magit-diff-mode)))
-    (kill-buffer magit-diff-buffer-in-current-repo)))
-;;
-;; When 'C-c C-c' is pressed in the magit commit message buffer,
-;; delete the magit-diff buffer related to the current repo.
-;;
-(add-hook 'git-commit-setup-hook
-          (lambda ()
-            (add-hook 'with-editor-post-finish-hook
-                      #'kill-magit-diff-buffer-in-current-repo
-                      nil t))) ; the t is important
-
-
-;; Buffer
-;; (setq buffer-read-only nil)
-;; (setq comint-prompt-read-only nil)
-
-;; Persp mode
-;;
-;; disable auto save (causes emacs to freeze on exit)
-;; (setq persp-auto-save-opt 0)
-(setq +workspaces-on-switch-project-behavior t)
-
-
-;; Ledger
-(use-package ledger-mode
-  :ensure t
-  :init
-  (setq ledger-clear-whole-transactions t)
-  :config
-  (add-to-list 'evil-emacs-state-modes 'ledger-report-mode)
-  :mode "\\.dat\\'"
-  :preface
-  (defun my/ledger-save ()
-    "Automatically clean the ledger buffer at each save."
-    (interactive)
-    (save-excursion
-      (when (buffer-modified-p)
-        (with-demoted-errors (ledger-mode-clean-buffer))
-        (save-buffer)))))
-
-(defun std::ledger::save ()
-  "First `ledger-mode-clean-buffer', then `save-buffer'."
-  (interactive)
-  (save-excursion
-    (when (buffer-modified-p)
-      (with-demoted-errors (ledger-mode-clean-buffer))
-      (save-buffer))))
--- snowblocks/doom/doomd/init.el
+++ snowblocks/doom/doomd/init.el
@@ -20,7 +20,6 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
@@ -31,6 +30,7 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;(emoji +unicode)  ; 🙂
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
@@ -39,7 +39,7 @@
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
-       neotree           ; a project drawer, like NERDTree for vim
+       ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
@@ -73,36 +73,36 @@
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       eshell            ; the elisp shell that works everywhere
+       ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
        ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       ;;spell             ; tasing you for misspelling mispelling
+       ;;(spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       debugger          ; FIXME stepping through code, to help you add bugs
        direnv
-       docker
+       ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ein               ; tame Jupyter notebooks with emacs
+       ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        lsp
        magit             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
-       tmux              ; an API for interacting with tmux
+       ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -113,7 +113,7 @@
        ;;agda              ; types of types of types of types...
        ;;cc                ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
-       common-lisp       ; if you've seen one lisp, you've seen them all
+       ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
@@ -131,8 +131,8 @@
        ;;(go +lsp)         ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
-       ;;idris             ;
-       json              ; At least it ain't XML
+       ;;idris             ; a language you can depend on
+       ;;json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
@@ -140,7 +140,7 @@
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
-       ledger            ; an accounting system in Emacs
+       ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
@@ -150,7 +150,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp -nosetests)            ; beautiful is better than ugly
+       python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -169,12 +169,14 @@
        yaml              ; JSON, but readable
 
        :email
-       (mu4e +gmail)
+       ;;(mu4e +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
        :app
        ;;calendar
+       ;;emms
+       ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
