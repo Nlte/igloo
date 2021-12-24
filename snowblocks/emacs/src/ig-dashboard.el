081e16c3f3bd9810ffe60cddb50ae01983f3cf39
Nlte <n.leaute@gmail.com>
2021-12-24 13:15:08 +0100

    Dashboards wip

---
+++ snowblocks/emacs/src/ig-dashboard.el
@@ -0,0 +1,86 @@
+;;; ig-dashboard.el --- summary -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+;;;###autoload
+(defmacro igloo-dashboard-define-minor-mode (name)
+  "Macro to define minor modes for dashboards."
+  `(define-minor-mode ,name
+     ,(format "%s" name)
+     :init nil
+     :lighter ,(format "%s" name)
+     :keymap (make-sparse-keymap)
+     (when ,name
+       (read-only-mode))))
+
+;;;###autoload
+(defmacro igloo-dashboard-define-key (key action)
+  "Macro to define a key for the dashboard."
+  (if (bound-and-true-p evil-mode)
+      (evil-define-key '(normal visual replace operator motion emacs)
+  'igloo-dashboard-mode "q" #'igloo-email-close)
+    (message "evil-mode is on")
+  (message "evil-mode is off")))
+
+
+;;;###autoload
+(defun igloo-dashboard-parse-modename ()
+  "Parse an org file for the minor mode name."
+  (org-element-map (org-element-parse-buffer) 'keyword
+    (lambda (k)
+      (when (string= (org-element-property :key k) "MODE")
+        (org-element-property :value k)))))
+
+
+;;;###autoload
+(defun igloo-dashboard-parse-and-set-keymap ()
+  "Parse an org file for keybindings.
+  eg. #+KEYMAP: q | kill-current-buffer
+"
+
+  (local-set-key (kbd "<return>") #'org-open-at-point)
+
+  (org-element-map (org-element-parse-buffer) 'keyword
+    (lambda (k)
+      (when (string= (org-element-property :key k) "KEYMAP")
+        (let* ((value (org-element-property :value k))
+               (key   (string-trim (nth 0 (split-string value "|"))))
+               (call  (string-trim (nth 1 (split-string value "|")))))
+          (local-set-key
+           (kbd key)
+           (eval (car (read-from-string
+                       (format "(lambda () (interactive) (%s))" call)))))
+          (message
+           "mu4e-dashboard: binding %s to %s"
+           key
+           (format "(lambda () (interactive) (%s))" call)))))))
+
+;;;###autoload
+(defun igloo-dashboard-parse-keymap ()
+  "Parse an org file for keybindings."
+  (org-element-map (org-element-parse-buffer) 'keyword
+    (lambda (k)
+        (when (string= (org-element-property :key k) "KEYMAP")
+        (let* ((value (org-element-property :value k))
+               (key   (string-trim (nth 0 (split-string value "|"))))
+               (call  (string-trim (nth 1 (split-string value "|")))))
+          (list key call))))))
+
+
+(defun igloo-dashboard-open (filename)
+  "Opens a dashboard file and configures mode and keybindings."
+  (find-file filename)
+  (let* ((modename (igloo-dashboard-parse-modename))
+         (keymap (igloo-dashboard-parse-keymap)))
+    (if (not modename)
+        (error (format "+MODE: undefined in dashboard '%s'" filename)))
+    (message modename)))
+    ;; (message keymap)
+
+
+(provide 'ig-dashboard)
+
+;;; ig-dashboard.el ends here
