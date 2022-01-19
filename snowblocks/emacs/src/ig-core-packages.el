2c9f9a126edc36bad1b05b5f275e531ed41b95a0
Nlte <n.leaute@gmail.com>
2022-01-19 08:47:39 +0100

    Native comp

--- snowblocks/emacs/src/ig-core-packages.el
+++ snowblocks/emacs/src/ig-core-packages.el
@@ -9,9 +9,26 @@
 
 (use-package exec-path-from-shell
   :straight t
-  :init
-  (when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize)))
+  :config
+  (exec-path-from-shell-initialize)
+  (if (and (fboundp 'native-comp-available-p)
+           (native-comp-available-p))
+      (progn
+	    (message "Native comp is available")
+        ;; Using Emacs.app/Contents/MacOS/bin since it was compiled with
+        ;; ./configure --prefix="$PWD/nextstep/Emacs.app/Contents/MacOS"
+        ;; Append to path to give priority to values from exec-path-from-shell-initialize.
+        (add-to-list 'exec-path (concat invocation-directory (file-name-as-directory "bin")) t)
+	    (setenv "LIBRARY_PATH" (concat (getenv "LIBRARY_PATH")
+                                       (when (getenv "LIBRARY_PATH")
+                                         ":")
+				                       ;; This is where Homebrew puts libgccjit libraries.
+                                       (car (file-expand-wildcards
+                                             (expand-file-name "/usr/local/opt/libgccjit/lib/gcc/*")))))
+	    ;; Only set after LIBRARY_PATH can find gcc libraries.
+	    (setq comp-deferred-compilation t)
+        (setq comp-speed 3))
+    (message "Native comp is *not* available")))
 
 (use-package interaction-log
   :straight t)
