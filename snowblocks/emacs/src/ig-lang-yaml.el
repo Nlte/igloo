47e95c1f66276b07032f0cdb7095d3c938465ff9
Nathanael Leaute <nathanael.leaute@man.com>
2022-02-16 07:57:07 +0000

    igloo-emacs update

---
+++ snowblocks/emacs/src/ig-lang-yaml.el
@@ -0,0 +1,15 @@
+;;; ig-lang-yaml.el --- summary -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(use-package yaml-mode
+  :straight t
+  :config
+  (setq yaml-indent-offset 2))
+
+(provide 'ig-lang-yaml)
+
+;;; ig-lang-yaml.el ends here
---
+++ snowblocks/emacs/src/ig-ssh.el
@@ -0,0 +1,8 @@
+(setq remote-file-name-inhibit-cache nil)
+(setq vc-ignore-dir-regexp
+      (format "%s\\|%s"
+              vc-ignore-dir-regexp
+              tramp-file-name-regexp))
+(setq tramp-verbose 10)
+
+(provide 'ig-ssh)
