e8939efe4072b3cedb8d1ea3442119027b0cc8b6
Nlte <nleaute@gmail.com>
2023-10-11 01:08:48 +0100

    Neovim lua

--- snowblocks/fish/config.fish
+++ snowblocks/fish/config.fish
@@ -15,41 +15,16 @@ set -gx FZF_DEFAULT_OPTS "
     --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b"
 set -gx RIPGREP_CONFIG_PATH $IGLOO_PATH_USER_CONFIG/ripgrep/config
 
-
-# locals
-set -gx LC_ALL en_US.UTF-8
-set -gx LANG en_US.UTF-8
-
-
 # path
-set PATH $HOME/Library/Python/3.7/bin $PATH
 set PATH $HOME/igloo/snowblocks/bin $PATH
-set PATH /usr/local/opt/llvm/bin $PATH
-set PATH /usr/local/opt/qt/bin $PATH
 set PATH $HOME/utils/binaries/ $PATH
-set PATH $HOME/miniconda3/bin/ $PATH
-set PATH $HOME/.roswell/bin $PATH
-set PATH $HOME/.cargo/bin $PATH
-
 
 # alias
 alias vim=nvim
 alias python=python3
 
-# direnv
-eval (direnv hook fish)
-
-# PATH
-fish_add_path /usr/local/opt/ruby/bin
-fish_add_path /usr/local/opt/openssl@1.1/bin
-export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
-export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
-export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
-export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
-
 # local environment variables
 set FILE "$HOME/.config/fish/local.fish" && test -f $FILE && source $FILE
-
 
 # Activate vi mode
 fish_vi_key_bindings
--- snowblocks/nvim/install.sh
+++ snowblocks/nvim/install.sh
@@ -5,11 +5,3 @@ if ! [ $? == 0 ]; then
 	echo "nvim not installed, aborting"
 	exit 1
 fi
-
-command -v npm > /dev/null
-if ! [ $? == 0 ]; then
-	echo "npm not installed, required for coc.nvim aborting"
-	exit 1
-fi
-
-
